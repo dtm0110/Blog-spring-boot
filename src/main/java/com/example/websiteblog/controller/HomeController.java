02f2356794386eb1ffa6f316525ddc250477c9f7
doantuanmanh <manhdt@animverse.com>
2022-05-24 20:21:32 +0700

    test

--- src/main/java/com/example/websiteblog/controller/HomeController.java
+++ src/main/java/com/example/websiteblog/controller/HomeController.java
@@ -2,14 +2,11 @@ package com.example.websiteblog.controller;
 
 import com.example.websiteblog.model.Post;
 import com.example.websiteblog.service.IPostService;
-import com.example.websiteblog.service.impl.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class HomeController {
@@ -19,7 +16,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model){
-
         List<Post> posts = iPostService.getAllPost();
         model.addAttribute("posts", posts);
         return "home";
